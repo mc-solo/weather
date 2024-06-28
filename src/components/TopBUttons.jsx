ba7751a35de3b27a634727f5885354a8ab1e4a58
wondwoesn <revatoel@gmail.com>
2024-06-28 04:10:17 +0300

    customize nav bar

---
+++ src/components/TopBUttons.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const TopBUttons = () => {
+    return (
+        <div className='flex flex-wrap items-center justify-around my-6'>
+            <button className='btn'>Ethiopia</button>
+            <button className='btn'>Ethiopia</button>
+            <button className='btn'>Ethiopia</button>
+            <button className='btn'>Ethiopia</button>
+            <button className='btn'>Ethiopia</button>
+
+
+        </div>
+    )
+}
+
+export default TopBUttons
