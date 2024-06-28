50e1d695d5a43f550bca56a19f732ebeb13820f4
wondwoesn <revatoel@gmail.com>
2024-06-28 05:02:17 +0300

    done with Input.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Inputs from './components/Inputs'
 
 const App = () => {
   return (
-    <div className='mx-auto max-w-screen-2xl min-w-full  py-4 px-20 bg-gradient-to-br shadow-xl shadow-gray-400 from-cyan-600 to-blue-700 text-2xl'>
+    <div className='mx-auto max-w-screen-2xl min-w-full  py-2 px-20 bg-gradient-to-br shadow-xl shadow-gray-400 from-cyan-600 to-blue-700 text-2xl'>
 
       <TopBUttons />
       <Inputs />
--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -3,7 +3,7 @@ import { BiSearch, BiCurrentLocation } from "react-icons/bi"
 const Inputs = () => {
     return (
         <div className='flex flex-row justify-center my-6'>
-            <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
+            <div className="flex flex-row w-2/3 items-center justify-center space-x-4">
                 <input
                     type="text"
                     placeholder="search city..."
@@ -19,6 +19,14 @@ const Inputs = () => {
                     className="cursor-pointer transition ease-out hover:scale-125" />
             </div>
 
+            <div className="flex flex-row w-1/4 items-center justify-center">
+
+                <button className="text-2xl font-medium transition ease-out hover:scale-125">°C
+                </button>
+                <p className="text-4xl px-[2px] font-medium mx-1">|</p>
+                <button className="text-2xl font-medium transition ease-out hover:scale-125">°F
+                </button>
+            </div>
 
         </div>
 
