f86ed0f2ffca4197fc03e88995dbde8022d112df
wondwoesn <revatoel@gmail.com>
2024-06-28 04:44:21 +0300

    add search and current location icons

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,14 +1,22 @@
+import { BiSearch, BiCurrentLocation } from "react-icons/bi"
+
 const Inputs = () => {
     return (
         <div className='flex flex-row justify-center my-6'>
-            <div>
+            <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
                 <input
                     type="text"
                     placeholder="search city..."
                     className="text-gray-500 text-xl font-light p-2 w-full shadow-xl capitalize focus:outline-none rounded-[4px]
                     placeholder:lowercase"
-
                 />
+
+                <BiSearch size={30}
+                    className="cursor-pointer transition ease-out hover:scale-125"
+                />
+
+                <BiCurrentLocation size={30}
+                    className="cursor-pointer transition ease-out hover:scale-125" />
             </div>
 
 
--- src/components/TopBUttons.jsx
+++ src/components/TopBUttons.jsx
@@ -3,9 +3,12 @@ import React from 'react'
 const TopBUttons = () => {
     return (
         <nav className='flex flex-wrap items-center justify-around my-6'>
-            <button className='btn'>Ethiopia</button>
-            <button className='btn'>Ethiopia</button>
-            <button className='btn'>Ethiopia</button>
+            <button className='btn'>Addis Ababa</button>
+            <button className='btn'>Sydney</button>
+            <button className='btn'>Denver</button>
+            <button className='btn'>Kanzas</button>
+            <button className='btn'>London</button>
+
 
 
         </nav>
