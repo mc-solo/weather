b1ba34a6cda7345a0b22a6af6f355af900268abd
wondwoesn <revatoel@gmail.com>
2024-07-04 10:19:19 +0300

    add the vertiacl details array

--- src/components/TempAndDetails.jsx
+++ src/components/TempAndDetails.jsx
@@ -5,6 +5,27 @@ import { MdKeyboardArrowUp } from "react-icons/md"
 import { GiSunrise, GiSunset } from "react-icons/gi"
 
 const TempAndDetails = () => {
+    const verticalDetails = [
+        {
+            id: 1,
+            Icon: FaThermometerEmpty,
+            title: 'Real Feel',
+            value: '22°'
+        },
+        {
+            id: 1,
+            Icon: FaThermometerEmpty,
+            title: 'Real Feel',
+            value: '22°'
+        }, {
+            id: 1,
+            Icon: FaThermometerEmpty,
+            title: 'Real Feel',
+            value: '22°'
+        }
+    ]
+
+
     return (
         <div>
             {/* first part */}
@@ -13,10 +34,23 @@ const TempAndDetails = () => {
             </div>
 
             {/* second part */}
-            <div className="flex flex-row items-center justify-between py-3
-            ">
+            <div className="flex flex-row items-center justify-between py-3">
 
-                <img src="http://openweathermap.org/img/wn/01d@2x.png" alt="weather icon" />
+                <img src="http://openweathermap.org/img/wn/01d@2x.png"
+                    alt="weather icon"
+                    className="w-20 "
+                />
+                <p className="text-5xl">34°</p>
+
+                <div className="flex flex-col space-y-3 items-start">
+
+                    <div className="flex font-light text-sm items- justify-center">
+                        <FaThermometerEmpty size={18} className="mr-1" />
+                        Real Feel: ,<span className="font-medium ml-1"> 34°   </span>
+                    </div>
+
+
+                </div>
 
             </div>
         </div>
