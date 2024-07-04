d99cb6dc69c2348742be851c4776a7d54841a98a
wondwoesn <revatoel@gmail.com>
2024-07-04 17:54:47 +0300

    successfully integrate theh open weather map API to the app

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,11 +1,13 @@
 const API_KEY = "60c0433e154c073b08503076cdc2b0da";
-const BASE_URL = "https://api.openweathermap.org/data/2.5 ";
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
 const getWeatherData = (infoType, searchParams) => {
-  const url = new URL(BASE_URL + infoType);
+  const url = new URL(BASE_URL + "/" + infoType);
   url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
 
   return fetch(url)
     .then((res) => res.json())
     .then((data) => data);
 };
+
+export default getWeatherData;
