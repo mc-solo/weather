7b370b9631fd9b0433f73f6a84ff6316acea1575
wondwoesn <revatoel@gmail.com>
2024-07-04 17:36:19 +0300

    add fetch api for test purpose

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -4,4 +4,8 @@ const BASE_URL = "https://api.openweathermap.org/data/2.5 ";
 const getWeatherData = (infoType, searchParams) => {
   const url = new URL(BASE_URL + infoType);
   url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
+
+  return fetch(url)
+    .then((res) => res.json())
+    .then((data) => data);
 };
