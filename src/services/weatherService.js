bb17f7d32808b6532cd7a0f345589e7afc8a483d
wondwoesn <revatoel@gmail.com>
2024-07-04 17:22:46 +0300

    add base url and setup an API key

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,2 +1,7 @@
-const API_KEY = "";
-const BASE_URL = "https://api.openweathermap.org/data/2.5";
+const API_KEY = "60c0433e154c073b08503076cdc2b0da";
+const BASE_URL = "https://api.openweathermap.org/data/2.5 ";
+
+const getWeatherData = (infoType, searchParams) => {
+  const url = new URL(BASE_URL + infoType);
+  url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
+};
