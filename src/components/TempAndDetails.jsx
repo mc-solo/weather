0f86142bb2f170da88e49e847fae73b111df6c5d
wondwoesn <revatoel@gmail.com>
2024-07-04 11:02:35 +0300

    implement horizontal component

--- src/components/TempAndDetails.jsx
+++ src/components/TempAndDetails.jsx
@@ -1,10 +1,12 @@
 import { FaThermometerEmpty } from "react-icons/fa"
 import { BiSolidDropletHalf } from "react-icons/bi"
 import { FiWind } from "react-icons/fi"
-import { MdKeyboardArrowUp } from "react-icons/md"
+import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md"
 import { GiSunrise, GiSunset } from "react-icons/gi"
 
 const TempAndDetails = () => {
+
+    // vertiacl details information
     const verticalDetails = [
         {
             id: 1,
@@ -13,17 +15,47 @@ const TempAndDetails = () => {
             value: '22°'
         },
         {
-            id: 1,
-            Icon: FaThermometerEmpty,
-            title: 'Real Feel',
-            value: '22°'
+            id: 2,
+            Icon: BiSolidDropletHalf,
+            title: 'Humidity',
+            value: '22%'
         }, {
-            id: 1,
-            Icon: FaThermometerEmpty,
-            title: 'Real Feel',
-            value: '22°'
+            id: 3,
+            Icon: FiWind,
+            title: 'wind',
+            value: '12km/h'
         }
+    ];
+
+
+    // horizontal details information
+    const horizontalDetails = [
+        {
+            id: 1,
+            Icon: GiSunrise,
+            title: 'Sunrise',
+            value: '05:33 AM'
+        },
+        {
+            id: 2,
+            Icon: GiSunset,
+            title: 'Sunset',
+            value: '08:33 PM'
+        },
+        {
+            id: 3,
+            Icon: MdKeyboardArrowUp,
+            title: 'High',
+            value: '37°'
+        },
+        {
+            id: 4,
+            Icon: MdKeyboardArrowDown,
+            title: 'Low',
+            value: '10°'
+        },
     ]
+
 
 
     return (
@@ -31,6 +63,7 @@ const TempAndDetails = () => {
             {/* first part */}
             <div className="flex items-center justify-center py-6 text-xl text-cyan-300">
                 <p>Rain</p>
+
             </div>
 
             {/* second part */}
@@ -44,15 +77,44 @@ const TempAndDetails = () => {
 
                 <div className="flex flex-col space-y-3 items-start">
 
-                    <div className="flex font-light text-sm items- justify-center">
-                        <FaThermometerEmpty size={18} className="mr-1" />
-                        Real Feel: ,<span className="font-medium ml-1"> 34°   </span>
-                    </div>
+                    {verticalDetails.map(({ id, Icon, title, value }) =>
+                        <div className="flex font-light text-sm items- justify-center"
+                            key={id}>
+                            <Icon size={18} className="mr-1" />
+                            {`${title}: `}
+                            <span className="font-medium ml-1"> {value}</span>
+                        </div>
+                    )}
 
 
                 </div>
-
             </div>
+
+
+            <div className="flex flex-row items-center justify-center space-x-10 text-sm py-3">
+
+
+                <div className="flex flex-row items-center space-x-20" >
+                    {
+                        horizontalDetails.map(({ id, Icon, title, value }) => (
+                            <div key={id} className="flex flex-row items-center">
+                                <Icon size={30} />
+                                <p className="font-light ml-1 ">
+                                    {`${title}: `}
+                                    <span className="font-medium ml-1"> {value}</span>
+                                </p>
+                            </div>
+
+
+                        )
+
+                        )
+                    }
+
+
+                </div>
+            </div>
+
         </div>
     )
 }
