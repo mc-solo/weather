2650f9d99882fbe46a766f3f445a39349494c467
wondwoesn <revatoel@gmail.com>
2024-07-04 10:03:01 +0300

    add open weather icon image

--- src/components/TempAndDetails.jsx
+++ src/components/TempAndDetails.jsx
@@ -7,7 +7,17 @@ import { GiSunrise, GiSunset } from "react-icons/gi"
 const TempAndDetails = () => {
     return (
         <div>
+            {/* first part */}
             <div className="flex items-center justify-center py-6 text-xl text-cyan-300">
+                <p>Rain</p>
+            </div>
+
+            {/* second part */}
+            <div className="flex flex-row items-center justify-between py-3
+            ">
+
+                <img src="http://openweathermap.org/img/wn/01d@2x.png" alt="weather icon" />
+
             </div>
         </div>
     )
