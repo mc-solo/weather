0c031009d7e0443e7419510a70b454c32a32959a
mc-solo <revatoel@gmail.com>
2024-07-16 00:44:08 +0300

    remove weatherService.js and related imports  from main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,10 @@ import Inputs from './components/Inputs'
 import TimeAndLocation from './components/TimeAndLocation'
 import TempAndDetails from './components/TempAndDetails'
 import Forecast from './components/Forecast'
-import getWeatherData from './services/weatherService'
 
 
 const App = () => {
 
-
-  const getWeather = async () => {
-    const data = await getWeatherData('weather', { q: 'berlin' })
-    console.log(data)
-  }
-
-  getWeather();
 
   return (
     <div className='mx-auto max-w-screen-2xl min-w-full  py-2 px-20 bg-gradient-to-br shadow-xl shadow-gray-400 from-cyan-600 to-blue-700 text-2xl'>
