8af8e9966d68d3a4e007752678db360f70e34fec
mc-solo <revatoel@gmail.com>
2024-07-16 02:16:00 +0300

    implement async fetch api call with appropriate error handling method

---
+++ src/services/weatherService.js
@@ -0,0 +1,21 @@
+const API_KEY = '60c0433e154c073b08503076cdc2b0da';
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/';
+
+const getWeatherData = async (infoType, searchParams) => {
+    const url = new URL(`${BASE_URL}${infoType}`);
+    url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Response was not okay!');
+        } else {
+            const data = await response.json();
+            return data;
+        }
+    } catch (error) {
+        console.warn('Error encountered during data fetch:', error.message);
+    }
+};
+
+export default getWeatherData;
