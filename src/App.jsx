6ffd8e314b2af41c815a106b280c38f7443ec6e8
mc-solo <revatoel@gmail.com>
2024-07-17 01:44:02 +0300

    populating dynamic data to the UI

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,37 @@ import TimeAndLocation from "./components/TimeAndLocation";
 import TempAndDetails from "./components/TempAndDetails";
 import Forecast from "./components/Forecast";
 import getFormattedWeatherData from "./services/weatherService";
+import { useState, useEffect } from "react";
 
 const App = () => {
+  const [query, setQuery] = useState({ q: "london" });
+  const [units, setUnits] = useState("metric");
+  const [weather, setWeather] = useState(null);
+
   const getWeather = async () => {
-    const data = await getFormattedWeatherData({ q: "berlin" });
-    console.log(data);
+    const data = await getFormattedWeatherData({ q: "berlin" }).then((data) => {
+      setWeather(data);
+    });
+    // console.log(data);
   };
 
+  useEffect(() => {
+    getWeather();
+  }, [query, units]);
   getWeather();
 
   return (
     <div className="mx-auto max-w-screen-2xl min-w-full  py-2 px-20 bg-gradient-to-br shadow-xl shadow-gray-400 from-cyan-600 to-blue-700 text-2xl">
       <TopButtons />
       <Inputs />
-      <TimeAndLocation />
-      <TempAndDetails />
-      <Forecast />
-      <Forecast />
+      {weather && (
+        <>
+          <TimeAndLocation weather={weather} />
+          <TempAndDetails weather={weather} />
+          <Forecast />
+          <Forecast />
+        </>
+      )}
     </div>
   );
 };
--- src/components/TimeAndLocation.jsx
+++ src/components/TimeAndLocation.jsx
@@ -1,18 +1,16 @@
-const TimeAndLocation = () => {
-    return (
-        <div>
-            <div className="flex items-center justify-center my-6">
+const TimeAndLocation = ({
+  weather: { formattedLocalTime, name, country },
+}) => {
+  return (
+    <div>
+      <div className="flex items-center justify-center my-6">
+        <p className="text-xl font-extralight">{formattedLocalTime}</p>
+      </div>
+      <div className="flex items-center justify-center my-3">
+        <p className=" text-3xl font-medium">{`${name}, ${country}`}</p>
+      </div>
+    </div>
+  );
+};
 
-                <p className="text-xl font-extralight">
-                    Thursday,28 Jun 2024 | Local Time 05:06 AM
-                </p>
-
-            </div>
-            <div className="flex items-center justify-center my-3">
-                <p className=" text-3xl font-medium">Berlin, DE</p>
-            </div>
-        </div>
-    )
-}
-
-export default TimeAndLocation
+export default TimeAndLocation;
