05b6a64d9d20a2bc48119120d66dfe0dd214a89a
mc-solo <revatoel@gmail.com>
2024-07-16 22:13:48 +0300

    fix issue with timezone

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-import React from 'react'
-import { FaReact } from 'react-icons/fa'
-import TopButtons from './components/TopButtons'
-import Inputs from './components/Inputs'
-import TimeAndLocation from './components/TimeAndLocation'
-import TempAndDetails from './components/TempAndDetails'
-import Forecast from './components/Forecast'
-import getWeatherData from './services/weatherService'
+import React from "react";
+import { FaReact } from "react-icons/fa";
+import TopButtons from "./components/TopButtons";
+import Inputs from "./components/Inputs";
+import TimeAndLocation from "./components/TimeAndLocation";
+import TempAndDetails from "./components/TempAndDetails";
+import Forecast from "./components/Forecast";
+import getWeatherData from "./services/weatherService";
 
 const App = () => {
   const getWeather = async () => {
-    const data = await getWeatherData('weather', {q:'berlin'})
-  }
+    const data = await getWeatherData("weather", { q: "berlin" });
+  };
 
   getWeather();
-  
-  return (
-    <div className='mx-auto max-w-screen-2xl min-w-full  py-2 px-20 bg-gradient-to-br shadow-xl shadow-gray-400 from-cyan-600 to-blue-700 text-2xl'>
 
+  return (
+    <div className="mx-auto max-w-screen-2xl min-w-full  py-2 px-20 bg-gradient-to-br shadow-xl shadow-gray-400 from-cyan-600 to-blue-700 text-2xl">
       <TopButtons />
       <Inputs />
       <TimeAndLocation />
@@ -24,9 +23,7 @@ const App = () => {
       <Forecast />
       <Forecast />
     </div>
-  )
-}
+  );
+};
 
-export default App
-
-
+export default App;
--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,4 +1,4 @@
-import { TbEmpathize } from "react-icons/tb";
+import { DateTime } from "luxon";
 
 const API_KEY = '60c0433e154c073b08503076cdc2b0da';
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/';
@@ -20,24 +20,43 @@ const getWeatherData = async (infoType, searchParams) => {
     }
 };
 
-export default getWeatherData;
+// Export default getWeatherData if needed
 
+// Formats the epoch to local time using the luxon lib
+const formatToLocalTime = (secs, offset, format = "cccc, dd LLL yyyy' | Local time: 'hh:mm a") => {
+    return DateTime.fromSeconds(secs + offset, { zone: 'utc' }).toFormat(format)
+}
 
 // this formats the current data from the api call
 const formatCurrent = (data) => {
     const {
         coord: { lat, lon },
         main: { temp, feels_like, temp_min, temp_max, humidity },
-        name, dt, sys: { country, sunrise, sunset }
+        name, dt, sys: { country, sunrise, sunset },
+        weather,
+        wind: { speed },
+        timezone,
     } = data;
 
-}
+    const { main: details, icon } = weather[0]
+    const formattedLocalTime = formatToLocalTime(dt, timezone)
 
-
-// this one gets the formatted data from the above function
-const getFormattedWeatherData = async (searchParams) => {
-    const formattedCurrentWeather = await getWeatherData(
-        'weather',
-        searchParams
-    ).then();
+    return {
+        lat, lon, temp, feels_like, temp_min, temp_max, humidity, name, country,
+        sunrise, sunset, details, icon, speed, formattedLocalTime
+    };
 };
+
+// Gets the formatted data from the above function
+const getFormattedWeatherData = async (searchParams) => {
+    const formattedCurrentWeather = await getWeatherData('weather', searchParams)
+
+    // Proper error handling
+    if (data) {
+        return formatCurrent(data);
+    } else {
+        throw new Error('Could not fetch weather data');
+    }
+};
+
+export default getFormattedWeatherData
